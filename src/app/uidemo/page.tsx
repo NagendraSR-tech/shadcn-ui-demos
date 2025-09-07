bee5a9a465eac8d84e7107e8b7519b8980191fdc
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-09-07 20:15:28 +0530

    initial commit with nextjs + shadcn

---
+++ src/app/uidemo/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function UIDemo() {
+  return (
+    <>
+      <div className="p-4">UI Demo</div>
+      <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
+        click me
+      </button>
+    </>
+  );
+}
