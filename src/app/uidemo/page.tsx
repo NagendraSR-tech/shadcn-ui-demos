ee51f0afdf811cdf06a7eb65226737239d9f81d3
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-09-07 22:18:45 +0530

    add variant with custom button

--- src/app/uidemo/page.tsx
+++ src/app/uidemo/page.tsx
@@ -9,7 +9,7 @@ export default function UIDemo() {
         <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
           click me
         </button>
-        <Button variant="default" size="sm">
+        <Button variant="primary_custom_button" size="sm">
           Click me (shadcn-ui)
         </Button>
       </div>
