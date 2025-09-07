ca0cf557275cb62236ecbb16e26a091b484c306b
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-09-07 20:44:45 +0530

    add shadcn button and component

--- src/app/uidemo/page.tsx
+++ src/app/uidemo/page.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function UIDemo() {
   return (
     <>
-      <div className="p-4">UI Demo</div>
-      <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
-        click me
-      </button>
+      <div className="flex flex-col gap-4 items-center min-h-screen">
+        <h1 className="p-4 text-2xl font-bold tracking-normal">UI Demo</h1>
+        <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
+          click me
+        </button>
+        <Button variant="default" size="sm">
+          Click me (shadcn-ui)
+        </Button>
+      </div>
     </>
   );
 }
