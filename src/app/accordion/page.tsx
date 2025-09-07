96d99d24644141a81d986b3f7dfc377d5e8db143
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-09-07 22:53:35 +0530

    add alert and alert-dialog

--- src/app/accordion/page.tsx
+++ src/app/accordion/page.tsx
@@ -4,10 +4,23 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Terminal } from "lucide-react";
 
 function AccordionPage() {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex flex-col justify-center items-center h-full gap-6">
       <div className="w-[600px] p-4">
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
@@ -17,6 +30,35 @@ function AccordionPage() {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
+      </div>
+
+      <div>
+        <Alert variant="default">
+          <Terminal />
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            You can add components and dependencies to your app using the cli.
+          </AlertDescription>
+        </Alert>
+      </div>
+
+      <div>
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </div>
   );
